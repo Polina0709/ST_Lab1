1fc29696c79249eaaa857d2e01df15a6f5effecf
Polina0709 <melnik.polya@gmail.com>
2026-02-27 13:39:26 +0200

    Clean main branch: keep only README

--- src/main/java/lab1/exception/InvalidLineException.java
+++
@@ -1,7 +0,0 @@
-package lab1.exception;
-
-public final class InvalidLineException extends LabInputException {
-    public InvalidLineException(String message, String fixHint) {
-        super(message, fixHint);
-    }
-}
